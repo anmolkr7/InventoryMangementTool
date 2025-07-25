00cb5e6a4b5cca398ef3805892a1993083e993b2
anmolkr7 <anmolojha952@gmail.com>
2025-07-25 20:45:08 +0530

    middleware to authenticate the requests

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,47 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/user.js';
+
+const authMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  
+  // console.log("\nVerifying Token in Middleware");
+  // console.log("Authorization Header:", authHeader);
+  
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Unauthorized: No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1];
+  
+  
+  // console.log("Token Received:", token);
+  // console.log("JWT Secret used for verification:", process.env.JWT_SECRET);
+ 
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
+  
+    console.log("Token Decoded Successfully:", decoded);
+    
+
+    const user = await User.findById(decoded.id);
+    
+    if (!user) {
+      console.log("User not found for ID:", decoded.userId);
+      return res.status(401).json({ message: 'Unauthorized: Invalid user' });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    
+    //console.error("!!! Token Verification Error !!!:", error.message);
+    
+    res.status(401).json({ message: 'Unauthorized: Invalid token', error: error.message });
+  }
+};
+
+export default authMiddleware;
