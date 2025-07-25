8cbb9af3c5a42e357173a1d039b2e23305d06534
anmolkr7 <anmolojha952@gmail.com>
2025-07-25 22:01:59 +0530

    removed api prefix from endpoint to match with script

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ import productRoutes from './routes/productRoutes.js';
 const app = express();
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/products', productRoutes);
+app.use(authRoutes);
+app.use(productRoutes);
 
 export default app;
