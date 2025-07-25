e88a719e184d35ef6f891386bf52f1ffdd943035
anmolkr7 <anmolojha952@gmail.com>
2025-07-25 20:51:34 +0530

    setup express app with routes

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import authRoutes from './routes/authRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+
+export default app;
